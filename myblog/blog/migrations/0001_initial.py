8a95e379827c2bc280a303224483b21398196260
IwonaZ <iwona.zdzieblo@gmail.com>
2021-12-07 10:26:30 +0100

    run linter

--- myblog/blog/migrations/0001_initial.py
+++ myblog/blog/migrations/0001_initial.py
@@ -16,13 +16,27 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Blog',
+            name="Blog",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=150)),
-                ('body', models.TextField()),
-                ('date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=150)),
+                ("body", models.TextField()),
+                ("date", models.DateTimeField(default=django.utils.timezone.now)),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,46 +2,55 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from .models import Blog
-from  django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 
 def home(request):
-    context = {
-        'posts': Blog.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+    context = {"posts": Blog.objects.all()}
+    return render(request, "blog/home.html", context)
+
 
 class PostListView(ListView):
     model = Blog
-    template_name = 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-date']
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date"]
     paginate_by = 5
+
 
 class UserPostListView(ListView):
     model = Blog
-    template_name = 'blog/user_posts.html'
-    context_object_name = 'posts'
+    template_name = "blog/user_posts.html"
+    context_object_name = "posts"
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_object_or_404(User,username=self.kwargs.get('username'))
-        return Blog.objects.filter(author=user).order_by('-date')
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Blog.objects.filter(author=user).order_by("-date")
+
 
 class PostDetailView(DetailView):
     model = Blog
 
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Blog
-    fields = ['title', 'body']
+    fields = ["title", "body"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Blog
-    fields = ['title', 'body']
+    fields = ["title", "body"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -53,9 +62,10 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Blog
-    success_url = '/'
+    success_url = "/"
 
     def test_func(self):
         post = self.get_object()
@@ -63,5 +73,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
+
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, "blog/about.html", {"title": "About"})
