2cf782a4565bcad22b20852c16766981dd88926b
IwonaZ <iwona.zdzieblo@gmail.com>
2021-12-07 12:06:05 +0100

    set up test for admin

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class AdminTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="testuser", password="password"
+        )
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_add_blog_post(self):
+        self.client.login(username="testuser", password="password")
+        response = self.client.post(
+            "/admin/blog/blog/add/",
+            {
+                "title": "TEST TITLE",
+                "body": "SOME BODY",
+                "date_0": "2021-12-07",
+                "date_1": "11:08:04",
+                "initial-date_0": "2021-12-07",
+                "initial-date_1": "11:08:04",
+                "author": self.user.id,
+                "_save": "SAVE",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/')
+        self.assertTrue('TEST TITLE' in str(response.content))
